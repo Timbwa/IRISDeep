10d997b68c7df3e10fec2e2972dbc7a17ce79265
khademi8 <e226942@metu.edu.tr>
2021-06-20 15:40:02 +0400

    Completion of acquisition (before David makes it pretty)

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import acquisition
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
 
+    acquisition.data_acquisition()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print('Done')
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
